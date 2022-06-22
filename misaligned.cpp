c03742fc92d9e3621d7a2bdf1cac4a1d0a241139
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-22 16:21:04 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -5,9 +5,9 @@ int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j + 1 << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
+    for(i = 1; i <= 5; i++) {
+        for(j = 1; j <= 5; j++) {
+            std::cout << (i-1) * 5 + j << " | " << majorColor[i-1] << " | " << minorColor[j-1] << "\n";
         }
     }
     return i * j;
