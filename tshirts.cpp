739a1570d5e42c1f308883d4efab588cdc082163
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-29 22:22:27 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "tshirts.h"
 
 char size(int cms) {
     char sizeName = '\0';
@@ -10,14 +11,4 @@ char size(int cms) {
         sizeName = 'L';
     }
     return sizeName;
-}
-
-int main() {
-    assert(size(37) == 'S');
-    assert(size(38) == 'S');
-    assert(size(38) == 'M');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
-    std::cout << "All is well (maybe!)\n";
-    return 0;
 }
