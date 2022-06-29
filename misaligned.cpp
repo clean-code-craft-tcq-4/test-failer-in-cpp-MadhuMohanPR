299a29402083c8e8a7668703ea5d0e807296d084
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-29 19:17:57 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include <assert.h>
 
-int printColorMap() {
+#define MAJORCOLORELEMENT 5  //MAJORCOLORELEMENT defines number of Major Color
+#define MINORCOLORELEMENT 5  //MINORCOLORELEMENT defines number of Minor Color
+
+std::string displayColorPairOnConsole(int majorIndex, int minorIndex)
+{
+    std::string colorPair;
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j + 1 << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
+
+    colorPair = std::to_string(majorIndex * MAJORCOLORELEMENT + minorIndex)  + " | " +  majorColor[majorIndex] + " | " + minorColor[minorIndex] << "\n";
+
+    return colorPair;
+}
+
+int printColorMap()
+{
+    for(int i = 0; i < MAJORCOLORELEMENT; i++)
+    {
+        for(int j = 0; j < MAJORCOLORELEMENT; j++)
+        {
+            std::cout << displayColorPairOnConsole(i, j) << std::endl;
         }
     }
     return i * j;
 }
 
-int main() {
+int main()
+{
     int result = printColorMap();
     assert(result == 25);
+    assert(displayColorPairOnConsole(1,1).compare("6 | Red | Orange"));
+    assert(displayColorPairOnConsole(2,3).compare("13 | Yellow | Slate"));
+    assert(displayColorPairOnConsole(1,0).compare("5 | Red | Blue"));
+    
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
