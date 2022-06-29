5bf402adea80e0f01081a7b05acd60c3a7fabecc
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-29 23:42:37 +0530

    Update alerter_test.cpp

--- alerter_test.cpp
+++ alerter_test.cpp
@@ -6,11 +6,11 @@ int main()
 {
     setalertertstub(200);
     alertInCelcius(400.5);
-    assert(alertFailureCount == 0);
+    assert(getAlertFailureCount() == 0);
 
     setalertertstub(500);
     alertInCelcius(303.6);
-    assert(alertFailureCount == 1);
+    assert(getAlertFailureCount() == 1);
 
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
