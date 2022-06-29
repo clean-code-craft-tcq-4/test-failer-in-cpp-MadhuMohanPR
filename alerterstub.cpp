67c5083c08b7b94384fa19a9f3962dfc7f62eb76
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-29 23:12:31 +0530

    Create alerterstub.cpp

---
+++ alerterstub.cpp
@@ -0,0 +1,10 @@
+#include "alerterstub.h"
+
+int networkAlertStub(float celcius)
+{
+    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    return 200;
+}
