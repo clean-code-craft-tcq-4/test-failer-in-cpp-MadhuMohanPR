6afdf50358689d63db5b1dd49fe996e95c0dbf93
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-29 22:44:01 +0530

    Create misaligned.h

---
+++ misaligned.h
@@ -0,0 +1,10 @@
+#ifndef TSHIRTS_H
+#define TSHIRTS_H
+
+#include <iostream>
+#include <assert.h>
+
+std::string displayColorPairOnConsole(int majorIndex, int minorIndex);
+int printColorMap();
+  
+#endif
