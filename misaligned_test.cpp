4788001cf8d05447d0d16c0d05bb9ec17ea76a8c
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-07-10 20:13:19 +0530

    Update misaligned_test.cpp

--- misaligned_test.cpp
+++ misaligned_test.cpp
@@ -9,11 +9,11 @@ int main()
     assert(result == 25);
 
     std::string expected = "0    | White      | Blue";
-    std::string received = printLine(0, 0);
+    std::string received = displayColorPairOnConsole(0, 0);
     assert(expected.compare(received)== 0);
 
     expected = "10   | Black      | Blue";
-    received = printLine(2, 0);
+    received = displayColorPairOnConsole(2, 0);
     assert(expected.compare(received)== 0);
 
    /* assert(displayColorPairOnConsole(1,1).compare("6 | Red | Orange"));
