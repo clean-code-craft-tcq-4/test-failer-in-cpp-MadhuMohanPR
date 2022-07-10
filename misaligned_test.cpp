b9c33381b1c0613ef33b175bb1cdbc930fa377e6
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-07-10 19:52:11 +0530

    Update misaligned_test.cpp

--- misaligned_test.cpp
+++ misaligned_test.cpp
@@ -7,10 +7,19 @@ int main()
 {
     int result = printColorMap();
     assert(result == 25);
-    assert(displayColorPairOnConsole(1,1).compare("6 | Red | Orange"));
+
+    std::string expected = "6    | Red      | Orange";
+    std::string result = displayColorPairOnConsole(1,1);
+    assert(expected.compare(result)== 0);
+ 
+    std::string expected = "13    | Yellow      | Slate";
+    std::string result = displayColorPairOnConsole(2,3);
+    assert(expected.compare(result)== 0);
+
+   /* assert(displayColorPairOnConsole(1,1).compare("6 | Red | Orange"));
     assert(displayColorPairOnConsole(2,3).compare("13 | Yellow | Slate"));
     assert(displayColorPairOnConsole(1,0).compare("5 | Red | Blue"));
-    
+    */
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
